06e964b87eb9afc04f8b48308e6f0b501fc7745a
clark525 <clark5.25@163.com>
2017-09-26 18:03:44 +0800

    first commit

--- app/src/main/java/com/cmsz/unitepay/webviewdemo/MainActivity.java
+++ app/src/main/java/com/cmsz/unitepay/webviewdemo/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         et = (EditText) findViewById(R.id.et);
 
     }
